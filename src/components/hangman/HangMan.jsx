6afb86ef1579d9dd11a72b629057e00d8d57d1ea
VitalijusS <v.stifanovicius@gmail.com>
2024-08-09 18:19:56 +0300

    git page fix?

--- src/components/hangman/HangMan.jsx
+++ src/components/hangman/HangMan.jsx
@@ -1,12 +1,12 @@
-import hang from '/hang.png';
-import img1 from '/1.png';
-import img2 from '/2.png';
-import img3 from '/3.png';
-import img4 from '/4.png';
-import img5 from '/5.png';
-import dead from '/dead.png';
-import img0 from '/0.png';
-import win from '/win.png';
+import hang from '../img/hang.png';
+import img1 from '../img/1.png';
+import img2 from '../img/2.png';
+import img3 from '../img/3.png';
+import img4 from '../img/4.png';
+import img5 from '../img/5.png';
+import dead from '../img/dead.png';
+import img0 from '../img/0.png';
+import win from '../img/win.png';
 
 export function HangMan(params){
 
