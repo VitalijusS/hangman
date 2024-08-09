cc9f352552577e64b23976d89f4550345ed5651c
VitalijusS <v.stifanovicius@gmail.com>
2024-08-09 16:53:06 +0300

    game over notification css

--- src/components/gameLost/GameLost.jsx
+++ src/components/gameLost/GameLost.jsx
@@ -3,8 +3,8 @@ import { randomWord } from "../wordGenerator/WordGenereator"
 export function GameLost(){
     return(
         <div className="gameOver">
-            <h3>You lost</h3>
-            <p className="gameLostText">The word was {randomWord}</p>
+            <h3 className="lost">You lost</h3>
+            <p className="gameLostText">The word was <span>{randomWord}</span></p>
             <button onClick={()=>window.location.reload()} className="restartBtn"> Play again</button>
         </div>
     )
--- src/components/gameWon/GameWon.jsx
+++ src/components/gameWon/GameWon.jsx
@@ -4,8 +4,8 @@ import { randomWord } from "../wordGenerator/WordGenereator"
 export function GameWon(){
     return(
         <div className="gameOver">
-            <h3>You won</h3>
-            <p className="gameLostText">The word was {randomWord}</p>
+            <h3 className="win">You won</h3>
+            <p className="gameWinText">The word was <span>{randomWord}</span></p>
             <button onClick={()=>window.location.reload()} className="restartBtn"> Play again</button>
         </div>
     )
